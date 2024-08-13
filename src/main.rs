452f2eba3a273e84e6d8c02cb49510dfc0d36ff4
Tyree Chrisp <chri.tyree@gmail.com>
2024-08-13 16:17:46 -0700

    Updated parser. TODO: optimize by using BufReader instead of reading in full file

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ struct Cli {
 
 fn main() {
     println!("Hello, world!");
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    let args =  Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args =  Cli::parse();
     
-    println!("pattern {:?}, path: {:?}", args.pattern, args.path)
+    println!("pattern {:?}, path: {:?}", args.pattern, args.path);
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
